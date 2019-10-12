ca43b23d742f22779cbc5976667e4ec330b51d4b
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2019-10-12 18:49:56 +0530

    Black Formatted

--- __preparing__.py
+++ __preparing__.py
@@ -1,9 +1,10 @@
 import numpy as np
 from keras.utils import np_utils
 
+
 def prep(input_data, output_data):
 
-    n_spectrum = 3 # color chanel (RGB)
+    n_spectrum = 3  # color chanel (RGB)
     weight = 80
     height = 80
     X = input_data.reshape([-1, n_spectrum, weight, height])
@@ -15,11 +16,10 @@ def prep(input_data, output_data):
     indexes = np.arange(4000)
     np.random.shuffle(indexes)
 
-    X_train = X[indexes].transpose([0,2,3,1])
+    X_train = X[indexes].transpose([0, 2, 3, 1])
     y_train = y[indexes]
 
     # normalization
     X_train = X_train / 255
 
-    return (X_train,y_train)
-    
+    return (X_train, y_train)
