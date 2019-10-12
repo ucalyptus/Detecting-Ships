26b717b2302994ea2fec63a68f72a3c71b32f6b2
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2019-10-12 18:49:02 +0530

    Black Formatted

--- __input__.py
+++ __input__.py
@@ -1,12 +1,16 @@
 import json, sys, random
 import numpy as np
+
+
 def data():
 
-    f = open(r'./input/shipsnet.json')
+    f = open(r"./input/shipsnet.json")
     dataset = json.load(f)
     f.close()
 
-    input_data = np.array(dataset['data']).astype('uint8')
-    output_data = np.array(dataset['labels']).astype('uint8')
+    input_data = np.array(dataset["data"]).astype("uint8")
+    output_data = np.array(dataset["labels"]).astype(
+        "uint8"
+    )
 
     return (input_data, output_data)
