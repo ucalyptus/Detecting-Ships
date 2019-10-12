97fea1c494c09762e5ae23384da27dd367bd300a
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2019-10-12 18:51:48 +0530

    Black Formatted

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import __preparing__ as pr
 import __training__ as tr
 import __result__ as rs
 
+
 def main():
     input_data, output_data = inp.data()
 
-    X_train, y_train = pr.prep(input_data,output_data)
+    X_train, y_train = pr.prep(input_data, output_data)
 
     model = tr.train(X_train, y_train)
 
-    path = './input/scenes/scenes/sfbay_1.png'
-    rs.detect(model,path)
+    path = "./input/scenes/scenes/sfbay_1.png"
+    rs.detect(model, path)
+
 
 if __name__ == "__main__":
     main()
